f18406fc82b4aed4579a129fcaed0beee0f5cdd9
aps358 <as358@njit.edu>
2021-11-07 12:50:23 -0500

    Updation

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,30 +1,33 @@
+
+
 """Testing the Calculator"""
 from calculator.main import Calculator
-
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+import pprint
+# def test_calculator_result():
+#     """testing calculator result is 0"""
+#     calc = Calculator()
+#     assert calc.result == 0
 
 def test_calculator_add():
     """Testing the Add function of the calculator"""
     #Arrange by instantiating the calc class
     calc = Calculator()
     #Act by calling the method to be tested
-    calc.add_number(4)
+    calc.add_number(4,2)
     #Assert that the results are correct
-    assert calc.result == 4
+    assert calc.get_result() == 6
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
+# def test_calculator_get_result():
+#     """Testing the Get result method of the calculator"""
+#     calc = Calculator()
+#     assert calc.get_result() == 0
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
-    calc.subtract_number(1)
+    calc.subtract_number(1,0)
     assert calc.get_result() == -1
+
 def test_calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
@@ -34,5 +37,5 @@ def test_calculator_multiply():
 def test_calculator_divide():
     """ tests multiplication of two numbers"""
     calc = Calculator()
-    result  = calc.divide_numbers(1,2)
+    result = calc.divide_numbers(1,2)
     assert result == 0.5
