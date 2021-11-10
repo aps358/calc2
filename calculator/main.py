d7c4c852a45c24c493215c648380dcce16e90d27
aps358 <as358@njit.edu>
2021-11-09 19:31:27 -0500

    Created Part 5

--- calculator/main.py
+++ calculator/main.py
@@ -12,7 +12,7 @@ class Calculator:
 
     @staticmethod
     def get_result_of_first_calculation_added_to_history():
-        return Calculator.history[0].getResult()
+        return Calculator.history[0].get_result()
 
     @staticmethod
     def clear_history():
@@ -31,7 +31,7 @@ class Calculator:
     @staticmethod
     def get_result_of_last_calculation_added_to_history():
         # -1 gets the last item added to the list automaticly and you can expect it to have the get result method
-        return Calculator.history[-1].getResult()
+        return Calculator.history[-1].get_result()
 
     @staticmethod
     def add_number(value_a, value_b):
