92c96b0c28ba4646afa127ec4a2011b4337e3dd7
aps358 <as358@njit.edu>
2021-11-09 21:21:10 -0500

    Updated Part 4

--- calc/calculator.py
+++ calc/calculator.py
@@ -13,21 +13,15 @@ class Calculator:
     @staticmethod
     #tuple allows me to pass in as many values as a I want
     def add_numbers(tuple_values: tuple):
-        """ adds list of numbers"""
-        Calculations.add_addition_calculation(tuple_values)
-        return True
+        """ adds list of numbers and returns an addition result object"""
+        return Calculations.add_addition_calculation(tuple_values)
+
     @staticmethod
     def subtract_numbers(tuple_values: tuple):
-        """ subtract a list of numbers from result"""
-        Calculations.add_subtraction_calculation(tuple_values)
-        return True
+        """ subtract a list of numbers from result and returns a subtraction result object"""
+        return Calculations.add_subtraction_calculation(tuple_values)
+
     @staticmethod
     def multiply_numbers(tuple_values: tuple):
-        """ multiplication number from result"""
-        Calculations.add_multiplication_calculation(tuple_values)
-        return True
-    @staticmethod
-    def divide_numbers(tuple_values: tuple):
-        """ division number from result"""
-        Calculations.add_division_calculation(tuple_values)
-        return True
+        """ multiplication number from result and returns a multiplication result object"""
+        return Calculations.add_multiplication_calculation(tuple_values)
